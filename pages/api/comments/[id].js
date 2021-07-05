a5ad805b95f0b7005de8a00902855d523353a026
ms77grz <ms77grz@gmail.com>
2021-07-05 18:58:07 +0300

    Adding Comments API Routes - finished

--- pages/api/comments/[id].js
+++ pages/api/comments/[id].js
@@ -2,8 +2,37 @@ export default function handler(req, res) {
   const id = req.query.id;
 
   if (req.method === 'POST') {
+    const { email, name, text } = req.body;
+
+    if (
+      !email.includes('@') ||
+      !name ||
+      name.trim() === '' ||
+      !text ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' });
+      return;
+    }
+
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+
+    console.log(newComment);
+
+    res.status(201).json({ message: 'Added comment.', comment: newComment });
   }
 
   if (req.method === 'GET') {
+    const dummyList = [
+      { id: 'c1', name: 'Alex', text: 'A first comment!' },
+      { id: 'c2', name: 'Blake', text: 'A second comment!' },
+    ];
+
+    res.status(200).json({ comments: dummyList });
   }
 }
