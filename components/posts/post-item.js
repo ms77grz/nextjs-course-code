36793214bb1427f27dff2e7484e1f02ef0ec3132
ms77grz <ms77grz@gmail.com>
2021-07-13 21:16:01 +0300

    Rendering Dummy Post Data - finished

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -15,10 +15,16 @@ export default function PostItem(props) {
 
   return (
     <li className={classes.post}>
-      <Link>
+      <Link href={slug}>
         <a>
           <div className={classes.image}>
-            <Image src={imagePath} alt={title} width={300} height={200} />
+            <Image
+              src={imagePath}
+              alt={title}
+              width={300}
+              height={200}
+              layout='responsive'
+            />
           </div>
           <div className={classes.content}>
             <h3>{title}</h3>
