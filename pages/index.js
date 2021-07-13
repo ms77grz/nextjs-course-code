180545a85b33e9f3ddd9cd261b85314e7d129b71
ms77grz <ms77grz@gmail.com>
2021-07-13 22:39:29 +0300

    Adding the "All Posts" Page - finished

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,9 @@
 import { Fragment } from 'react';
 import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
+import DUMMY_POSTS from '../data/dummy-posts';
 
 export default function HomePage() {
-  const DUMMY_POSTS = [
-    {
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production – it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2021-07-03',
-      slug: 'getting-started-with-nextjs-1',
-    },
-    {
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production – it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2021-07-03',
-      slug: 'getting-started-with-nextjs-2',
-    },
-    {
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production – it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2021-07-03',
-      slug: 'getting-started-with-nextjs-3',
-    },
-    {
-      title: 'Getting Started with NextJS',
-      image: 'getting-started-nextjs.png',
-      excerpt:
-        'NextJS is a the React framework for production – it makes building fullstack React apps and sites a breeze and ships with built-in SSR.',
-      date: '2021-07-03',
-      slug: 'getting-started-with-nextjs-4',
-    },
-  ];
-
   return (
     <Fragment>
       <Hero />
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,6 @@
+import DUMMY_POSTS from '../../data/dummy-posts';
+import AllPosts from '../../components/posts/all-posts';
+
 export default function AllPostsPage() {
-  return <div></div>;
+  return <AllPosts posts={DUMMY_POSTS} />;
 }
