22925d2dd76c8014c9b7bf3d767a5671d2d95f5c
ms77grz <ms77grz@gmail.com>
2021-07-23 11:43:16 +0300

    Rendering Code Snippets From Markdown - finished

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,6 +2,8 @@ import ReactMarkdown from 'react-markdown';
 import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import Image from 'next/image';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export default function PostContent({ post }) {
   const { title, image, slug, content } = post;
@@ -27,6 +29,17 @@ export default function PostContent({ post }) {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      const language = className.split('-')[1];
+      return (
+        <SyntaxHighlighter
+          language={language}
+          children={children}
+          style={atomDark}
+        />
+      );
     },
   };
 
