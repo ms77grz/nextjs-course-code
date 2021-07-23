a3dc0c96c73c029b6e9ecde024a59b00cc095b1d
ms77grz <ms77grz@gmail.com>
2021-07-23 20:31:09 +0300

    Storing Messages With MongoDB In A Database - finished

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { connectDatabase, insertDocument } from '../../lib/db-util';
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
     if (
@@ -16,12 +18,31 @@ export default function handler(req, res) {
     // Store it in a database
     const newMessage = { email, name, message };
 
-    console.log(newMessage);
+    // Connect to MongoDB
 
-    res.status(201).json({
-      message: 'Successfully stored message!',
-      newMessage,
-    });
+    let client;
+
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
+
+    // Insert document to MongoDB
+
+    try {
+      await insertDocument(client, 'blog', 'messages', newMessage);
+      res
+        .status(201)
+        .json({ message: 'Added message.', newMessage: newMessage });
+      client.close();
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Inserting message failed!' });
+      return;
+    }
+  } else {
+    res.status(422).json({ message: 'It is not a POST request!' });
   }
-  res.status(422).json({ message: 'It is not a POST request!' });
 }
