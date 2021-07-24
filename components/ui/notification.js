f0090024c5245d80f18cdbd6b2e9fc9b03e0bdb5
ms77grz <ms77grz@gmail.com>
2021-07-24 17:04:18 +0300

    Using React Portals - finished

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,4 +1,5 @@
 import classes from './notification.module.css';
+import ReactDom from 'react-dom';
 
 function Notification(props) {
   const { title, message, status } = props;
@@ -15,11 +16,12 @@ function Notification(props) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDom.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById('notifications')
   );
 }
 
