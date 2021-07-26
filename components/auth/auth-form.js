2d3092f90dd30e2de797bcfeb395d5478eded86e
ms77grz <ms77grz@gmail.com>
2021-07-26 16:36:40 +0300

    Sending a "Signin" Request From The Frontend - finished

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react';
 import classes from './auth-form.module.css';
+import { signIn } from 'next-auth/client';
 
 async function createUser(email, password) {
   const response = await fetch('/api/auth/signup', {
@@ -38,7 +39,13 @@ function AuthForm() {
     // optional: add validation
 
     if (isLogin) {
-      // log in user
+      const result = await signIn('credentials', {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
